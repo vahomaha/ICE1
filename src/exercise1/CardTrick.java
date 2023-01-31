a212939f15f011e5cff23f176ed95738f3a3301a
saad <s4saadvhora1111@gmail.com>
2023-01-31 11:50:44 -0500

    class exercise

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,39 +1,34 @@
 package exercise1;
 
-/**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
- *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
- */
 public class CardTrick {
+  public static void main(String[] args) {
+    int[] deck = new int[52];
+    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9",
+      "10", "Jack", "Queen", "King"};
+        
+    // Initialize cards
+    for (int i = 0; i < deck.length; i++)
+      deck[i] = i;
     
-    public static void main(String[] args) {
-        
-        Card[] hand = new Card[7];
-
-        for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
-        }
-
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
-        
-        // If the guess is successful, invoke the printInfo() method below.
-        
+    // Shuffle the cards
+    for (int i = 0; i < deck.length; i++) {
+      // Generate an index randomly
+      int index = (int)(Math.random() * deck.length);
+      int temp = deck[i];
+      deck[i] = deck[index]; 
+      deck[index] = temp;
     }
+
+    // Display the first four cards
+    for (int i = 0; i < 4; i++) {
+      String suit = suits[deck[i] / 13];
+      String rank = ranks[deck[i] % 13];
+      System.out.println("Card number " + deck[i] + ": " 
+        + rank + " of " + suit);
+    }
+  }
+}
 
     /**
      * A simple method to print out personal information. Follow the instructions to 
